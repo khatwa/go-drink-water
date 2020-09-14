1609680296e269ec9a972df441b9fea95edd6c5a
Musab Khunaijir <musabkhunaijir@gmail.com>
2020-09-14 08:36:48 +0200

    feat: time config

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,40 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log(
+    'Congratulations, your extension "go-drink-water" is now active!'
+  );
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "go-drink-water" is now active!');
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  let disposable = vscode.commands.registerCommand(
+    "go-drink-water.drink",
+    () => {
+      // The code you place here will be executed every time your command is executed
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('go-drink-water.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+      const time: number = Number(
+        vscode.workspace.getConfiguration().get("go-drink-water.time")
+      );
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from go drink water!');
-	});
+      const timeInMinutes: number = time * 60000;
 
-	context.subscriptions.push(disposable);
+      // Display a message box to the user
+      vscode.window.showInformationMessage(`ur time every: ${time}`);
+
+      setInterval(() => {
+        vscode.window.showInformationMessage(`ur time every: ${time} minute`);
+      }, timeInMinutes);
+    }
+  );
+
+  context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
